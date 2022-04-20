aed5bea1ca9cfb805acb663a671ef01e64c56f83
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-20 15:59:37 +0100

    module resolver config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ["."],
+      extensions: [
+        '.Js',
+        '.jsx',
+        '.json',
+        '.ts',
+        '.tsx'
+      ],
+      alias: {
+        '@components': './src/components',
+        '@utils': './src/utils',
+        '@images': './src/assets/images',
+        '@screens': './src/screens',
+        '@hooks': './src/hooks',
+        '@contexts': './src/contexts',
+        '@store': './src/store',
+        '@types': './src/types',
+        '@configs': './src/configs',
+        '@services': './src/services',
+
+      }
+    }], 'react-native-reanimated/plugin'
+  ]
 };
