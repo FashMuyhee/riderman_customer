9ac4a56495b61f916260ad1f5ce907f7bcea8bc8
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-21 23:27:29 +0100

    added reset ,forget password and register

--- src/navigations/stack/auth.tsx
+++ src/navigations/stack/auth.tsx
@@ -1,6 +1,13 @@
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import ForgetPassword from '@screens/auth/forget-password';
 import Login from '@screens/auth/login';
+import Register from '@screens/auth/register';
+import ResetPassword from '@screens/auth/reset-password';
 import React from 'react';
+import {Platform} from 'react-native';
 import {AuthStackParamList} from '../param-list';
 
 const StackNav = createStackNavigator<AuthStackParamList>();
@@ -9,8 +16,18 @@ const AuthStack = () => {
   return (
     <StackNav.Navigator
       initialRouteName="login"
-      screenOptions={{headerShown: false}}>
+      screenOptions={{
+        headerShown: false,
+        presentation: 'card',
+        cardStyleInterpolator: Platform.select({
+          android: CardStyleInterpolators.forFadeFromBottomAndroid,
+          ios: CardStyleInterpolators.forHorizontalIOS,
+        }),
+      }}>
       <StackNav.Screen component={Login} name="login" />
+      <StackNav.Screen component={ForgetPassword} name="f_password" />
+      <StackNav.Screen component={ResetPassword} name="r_password" />
+      <StackNav.Screen component={Register} name="register" />
     </StackNav.Navigator>
   );
 };
