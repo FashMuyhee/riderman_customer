e785636b7b8d794ececf34eceed8f8e416a5a5e6
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-26 00:06:55 +0100

    init main stack

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -1,11 +1,12 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import AuthStack from './stack/auth';
+import GuardStack from './stack/guard';
 
 const RootNavigator = () => {
   return (
     <NavigationContainer>
-      <AuthStack />
+      <GuardStack />
     </NavigationContainer>
   );
 };
