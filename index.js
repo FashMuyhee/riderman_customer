d130d56a1b0a9f7e266d3e7c535372ce85fb4fb3
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-26 00:06:05 +0100

    map init

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ import { AppRegistry } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import App from './App';
 import { name as appName } from './app.json';
+import { enableLatestRenderer } from 'react-native-maps';
+enableLatestRenderer();
 enableScreens()
 AppRegistry.registerComponent(appName, () => App);
