3cb87399fe5f7698a3d35e6be52ed962ce86c172
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-26 22:09:34 +0100

    bike marker

---
+++ src/utils/sample-data.ts
@@ -0,0 +1,39 @@
+export const
+  bikes = [
+    {
+      id: '0',
+      heading: 47,
+      latitude: 6.545226363494771,
+      longitude: 3.395496993633008,
+    },
+    {
+      id: '1',
+      latitude: 6.544355082389553,
+      longitude: 3.395295243282711,
+      heading: 190,
+    },
+    {
+      id: '2',
+      latitude: 6.545222272976703,
+      longitude: 3.3950632990024383,
+      heading: 99,
+    },
+    {
+      id: '3',
+      latitude: 6.544679597282447,
+      longitude: 3.3953528862399387,
+      heading: 120,
+    },
+    {
+      id: '4',
+      latitude: 6.544725956535644,
+      longitude: 3.394853313943966,
+      heading: 120,
+    },
+    {
+      id: '5',
+      latitude: 6.545654504207982,
+      longitude: 3.3955299324844184,
+      heading: 120,
+    }
+  ]
