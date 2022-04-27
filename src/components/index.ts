0e505c970a4e7829829c9756f794768afd22a5ed
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-27 12:02:29 +0100

    phone input component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,11 @@
 import Button from "./Button";
 import MultiSelectInput from "./MutilSelectInput";
 import PasswordTextInput from "./PasswordInput";
+import PhoneInput from "./PhoneInput";
 import ScreenWrapper from "./ScreenWrapper";
 import TextInput from "./TextInput";
 import TransparentNavbar from "./TransparentNavbar";
 
 
-export { ScreenWrapper, TextInput, PasswordTextInput, Button, TransparentNavbar, MultiSelectInput }
+export { ScreenWrapper, TextInput, PasswordTextInput, Button, TransparentNavbar, MultiSelectInput, PhoneInput }
 
