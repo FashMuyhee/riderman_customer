67749b8f4f520186599028242c4085395dd3477e
Fash Muyhee <fashmuyidavid@gmail.com>
2022-04-28 09:43:39 +0100

    bottomsheet init

---
+++ src/components/BottomSheetWrapper.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import {View, Text} from 'native-base';
+import {hp} from '@utils/responsive';
+
+export type IBottomSheetWrapperProps = {
+  isBackDrop?: boolean;
+  height?: number;
+  children: React.ReactElement;
+};
+
+const BottomSheetWrapper = React.forwardRef<RBSheet, IBottomSheetWrapperProps>(
+  ({children, isBackDrop = true, height = hp(45)}, ref) => {
+    return (
+      <RBSheet
+        closeOnPressBack
+        keyboardAvoidingViewEnabled
+        ref={ref}
+        height={height}
+        closeOnDragDown
+        closeOnPressMask={false}
+        customStyles={{
+          wrapper: {
+            backgroundColor: isBackDrop
+              ? 'rgba(18, 18, 18, 0.3)'
+              : 'transparent',
+          },
+          draggableIcon: {
+            backgroundColor: '#f5f5f5',
+            width: 60,
+          },
+          container: {
+            borderTopRightRadius: 25,
+            borderTopLeftRadius: 25,
+          },
+        }}>
+        {children}
+      </RBSheet>
+    );
+  },
+);
+
+export default BottomSheetWrapper;
