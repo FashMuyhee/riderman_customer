b3db6714f61b39e502108f1a6fc04dbcd1a43367
FashMuyhee <fashmuyidavid@gmail.com>
2022-05-20 09:46:58 +0100

    init pressable input

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,10 +4,11 @@ import FlatList from "./FlatList";
 import MultiSelectInput from "./MutilSelectInput";
 import PasswordTextInput from "./PasswordInput";
 import PhoneInput from "./PhoneInput";
+import { PressableInput } from "./PressableInput";
 import ScreenWrapper from "./ScreenWrapper";
 import TextInput from "./TextInput";
 import TransparentNavbar from "./TransparentNavbar";
 
 
-export { ScreenWrapper, TextInput, PasswordTextInput, Button, TransparentNavbar, MultiSelectInput, PhoneInput, BottomSheetWrapper, FlatList, BottomSheetWrapperSnappy }
+export { ScreenWrapper, TextInput, PasswordTextInput, Button, TransparentNavbar, MultiSelectInput, PhoneInput, BottomSheetWrapper, FlatList, BottomSheetWrapperSnappy, PressableInput }
 
