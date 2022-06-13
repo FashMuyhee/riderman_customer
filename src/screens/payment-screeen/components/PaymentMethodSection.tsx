ccfcbf80190d6a9049613f7773c91d2b5217b575
FashMuyhee <fashmuyidavid@gmail.com>
2022-06-13 21:32:47 +0100

    init payment screen

---
+++ src/screens/payment-screeen/components/PaymentMethodSection.tsx
@@ -0,0 +1,50 @@
+import {View, Text, Center, HStack} from 'native-base';
+import React from 'react';
+import {PaymentMethod} from '@models/delivery';
+import {Image, ImageBackground} from 'react-native';
+import walletBg from '@images/wallet-bg.png';
+import {MoneyText} from '@components';
+import cash from '@images/illustrations/cash-on-delivery.png';
+import InfoGradient from '@components/icons/info-gradient';
+
+type Props = {
+  method: PaymentMethod;
+};
+
+const PaymentMethodSection = ({method}: Props) => {
+  const Wallet = () => {
+    return (
+      <View w="full" h="100px">
+        <ImageBackground source={walletBg} style={{width: '100%', height: '100%', justifyContent: 'center'}} resizeMode="contain">
+          <Center>
+            <Text fontSize={'13px'} color="white">
+              Wallet Balance
+            </Text>
+            <MoneyText moneyValue={50000} fontWeight="600" fontSize="20px" color="white" />
+          </Center>
+        </ImageBackground>
+      </View>
+    );
+  };
+
+  const Cash = () => {
+    return (
+      <View w="full">
+        <Image source={cash} style={{alignSelf: 'center', height: 70, width: 100, resizeMode: 'contain'}} />
+        <HStack justifyContent="center" alignItems="center" my="5px" space="2">
+          <InfoGradient />
+          <Text fontSize="12px" flexWrap="wrap" w="80%">
+            Rider has to confirm from his application that full payment has been made in cash.
+          </Text>
+        </HStack>
+      </View>
+    );
+  };
+  return (
+    <View w="full">
+      <Cash />
+    </View>
+  );
+};
+
+export default PaymentMethodSection;
---
+++ src/screens/payment-screeen/index.tsx
@@ -0,0 +1,93 @@
+import {MoneyText, ScreenWrapper, Button, DashedDivider} from '@components';
+import {GuardStackParamList} from '@navigations/param-list';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {View, Text, Image, ScrollView, Box, HStack} from 'native-base';
+import React from 'react';
+import {hp} from '@utils/responsive';
+import logo from '@images/company-logo.png';
+import rider from '@images/rider.png';
+import TimeSolid from '@components/icons/time-solid';
+import {PaymentMethodIcon} from '../request-delivery/components/SelectPaymentMethod';
+import RiderBrief from '../request-delivery/components/RiderBrief';
+import RequestLocations from '../request-delivery/components/RequestLocations';
+import {PackageBrief} from '../request-delivery/request-preview';
+import {moneyFormat} from '@components/MoneyText';
+import PaymentMethodSection from './components/PaymentMethodSection';
+
+interface IProps {
+  navigation: StackNavigationProp<GuardStackParamList, 'payment_screen'>;
+}
+
+const PaymentScreen = ({navigation}: IProps) => {
+  const packageBrief = [
+    {
+      contactPhone: '0801 234 5678',
+      contactName: 'Jonathan Jude',
+      packageType: ['Cloth', 'Food'],
+    },
+    {
+      contactPhone: '0801 234 5678',
+      contactName: 'Nancy Wheeler',
+      packageType: ['Bag', 'Shoe'],
+    },
+  ];
+
+  return (
+    <ScreenWrapper barColor="white" barStyle="dark-content">
+      <ScrollView bounces={false}>
+        <View h={hp(100)} w="full" bg="coolGray.200" />
+        <View px="10px" pt="10px" bg="white" position="absolute" bottom="0" borderTopRadius="2xl" w="full" minH={hp(70)}>
+          <View my="10px" px="20px">
+            <Text textAlign="center" bold mb="4px">
+              RIDER HAS ARRIVED FOR PICKUP!
+            </Text>
+            <Text textAlign="center" fontSize={hp(1.3)}>
+              To activate delivery tracking, you will need to make payment for the dispatcher’s service
+            </Text>
+          </View>
+          <PaymentMethodSection method="Cash" />
+          <Box>
+            {/* comoany info */}
+            <DashedDivider />
+            <HStack h="60px" alignItems="center" justifyContent="space-between" px="10px">
+              <Image source={logo} alt="company logo" rounded="full" bg="gray.400" size="50px" />
+              <View w="60%">
+                <Text fontSize={hp(1.3)} fontWeight="600">
+                  Rush Delivery Limited
+                </Text>
+                <HStack mt="10px" alignItems="center" space="2">
+                  <TimeSolid />
+                  <Text fontSize="12px">3 min away</Text>
+                </HStack>
+              </View>
+              <View justifyContent="center" alignItems="center" w="20%">
+                <HStack alignItems="center" space="1">
+                  <PaymentMethodIcon method="Cash" selected={false} />
+                  <Text>Cash</Text>
+                </HStack>
+                <MoneyText bold moneyValue={6000} />
+              </View>
+            </HStack>
+            <DashedDivider />
+            {/* rider info */}
+            <RiderBrief image={rider} fullname="Adeola Adebimpe" plateNo="GTY67809" rating={3} />
+            <RequestLocations pickUp="26, Obafemi Awolowo Road" deliveryLocations={['Murtala Mohammed Internationational Airport Lagos', 'Bayeku Igbogbo Ikorodu, Ikorodu, Lagos']} />
+            <DashedDivider />
+            <ScrollView h="110px" showsVerticalScrollIndicator={false}>
+              {packageBrief.map((item, key) => (
+                <PackageBrief key={key} {...item} index={key + 1} />
+              ))}
+            </ScrollView>
+            <DashedDivider />
+          </Box>
+          <HStack alignItems="center" justifyContent="space-between" mb={hp(5)} mt="5%" px="10px">
+            <Button bg="black" title="Cancel Pickup" w="48%" onPress={() => navigation.navigate('home')} />
+            <Button title={`Pay \u20A6 ${moneyFormat(6000)}`} w="48%" />
+          </HStack>
+        </View>
+      </ScrollView>
+    </ScreenWrapper>
+  );
+};
+
+export default PaymentScreen;
