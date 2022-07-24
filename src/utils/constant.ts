e97bcd9c7d6260fd12ae24ead92f6c020a4de5d2
Fash Muyhee <fashmuyidavid@gmail.com>
2022-07-24 21:06:49 +0100

    added const statusbar height

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,6 @@
+import { StatusBar } from "react-native";
+
 export const INPUT_HEIGHT = '40px';
 export const FONT_FAMILY = 'Poppins'
+// @ts-ignore
+export const STATUSBAR_HEIGHT = StatusBar.currentHeight + 2
