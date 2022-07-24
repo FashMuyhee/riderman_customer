dd2d9860088fded3c1f748217c982cbfb75261de
Fash Muyhee <fashmuyidavid@gmail.com>
2022-07-24 22:54:31 +0100

    reduce inut height

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,7 @@
 import { StatusBar } from "react-native";
 
-export const INPUT_HEIGHT = '40px';
+export const INPUT_HEIGHT = '35px';
 export const FONT_FAMILY = 'Poppins'
 // @ts-ignore
 export const STATUSBAR_HEIGHT = StatusBar.currentHeight + 2
+export const RATINGS_WORDS = ['Meh', 'OK', 'Good', 'Very Good', 'Amazing']
