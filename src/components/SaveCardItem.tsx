2496716f501c94acdea00368853e4e687fe6adc2
Fash Muyhee <fashmuyidavid@gmail.com>
2022-07-24 14:26:01 +0100

    finalized saved card item

--- src/components/SaveCardItem.tsx
+++ src/components/SaveCardItem.tsx
@@ -1,16 +1,34 @@
-import { View, Text } from 'native-base'
-import React from 'react'
-import visa from '@images/visa.png'
-import mastercard from '@images/mastercard.png'
+import detectCard from '@utils/detect-card';
+import {View, HStack, Text} from 'native-base';
+import React from 'react';
+import CreditCardLogo from './CreditCardLogo';
+import Checkbox from './icons/checkbox';
 
-type Props = {}
+type IProps = {
+  number: string;
+  expiry: string;
+  selected: boolean;
+};
 
-const SaveCardItem = (props: Props) => {
+const SaveCardItem = ({expiry, number, selected}: IProps) => {
   return (
-    <View>
-      <Text>SaveCardItem</Text>
-    </View>
-  )
-}
+    <HStack alignItems="center" w="full" py="5px" px="10px" h="60px" borderWidth={selected ? 1.5 : 0} borderColor="main" rounded="md">
+      <CreditCardLogo creditCardNumber={number} />
+      <View w="78%" ml="20px">
+        <Text fontWeight="600" fontSize="13px">
+          {detectCard(number)?.formattedValue}
+        </Text>
+        <Text fontSize="11px" fontWeight="light" color="grey.200">
+          Expires {expiry}
+        </Text>
+      </View>
+      {selected && (
+        <View alignSelf="center">
+          <Checkbox />
+        </View>
+      )}
+    </HStack>
+  );
+};
 
-export default SaveCardItem
+export default SaveCardItem;
