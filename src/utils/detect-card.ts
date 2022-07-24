d3d5899333d98be85dbc76de51b98763175dd520
Fash Muyhee <fashmuyidavid@gmail.com>
2022-07-24 14:28:26 +0100

    clean ups

--- src/utils/detect-card.ts
+++ src/utils/detect-card.ts
@@ -1,46 +1,48 @@
 
-type Card = 'VISA' | 'MASTERCARD' | 'AMEX' | 'DINERS' | 'DISCOVER' | 'JCB'
+type Cards = 'VISA' | 'MASTERCARD' | 'AMEX' | 'DINERS' | 'DISCOVER' | 'JCB'
 
+interface ICard {
+  cardType: Cards;
+  formattedValue: string
+}
 /**
  * Detect card type from input number
  * @param cc 
  * @returns Card
  */
-const detectCard = (cc: string): Card | undefined => {
-  let amex = new RegExp('^3[47][0-9]{13}$');
+const detectCard = (cc: string): ICard | undefined => {
   let visa = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
-
   let mastercard = new RegExp('^5[1-5][0-9]{14}$');
   let mastercard2 = new RegExp('^2[2-7][0-9]{14}$');
 
-  let disco1 = new RegExp('^6011[0-9]{12}[0-9]*$');
-  let disco2 = new RegExp('^62[24568][0-9]{13}[0-9]*$');
-  let disco3 = new RegExp('^6[45][0-9]{14}[0-9]*$');
+  const regEx = /^(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})$/g
 
-  let diners = new RegExp('^3[0689][0-9]{12}[0-9]*$');
-  let jcb = new RegExp('^35[0-9]{14}[0-9]*$');
-
+  const formattedValue = cc.replace(regEx, (regEx, $1, $2, $3, $4) =>
+    [$1, $2, $3, $4].filter(group => !!group).join(' ')
+  )
 
   if (visa.test(cc)) {
-    return 'VISA';
+    return {
+      cardType: 'VISA',
+      formattedValue
+    }
   }
-  if (amex.test(cc)) {
-    return 'AMEX';
-  }
+
   if (mastercard.test(cc) || mastercard2.test(cc)) {
-    return 'MASTERCARD';
-  }
-  if (disco1.test(cc) || disco2.test(cc) || disco3.test(cc)) {
-    return 'DISCOVER';
-  }
-  if (diners.test(cc)) {
-    return 'DINERS';
-  }
-  if (jcb.test(cc)) {
-    return 'JCB';
+    return {
+      cardType: 'MASTERCARD',
+      formattedValue
+    }
   }
 
   return undefined;
 
 };
 export default detectCard
+
+export const maskCreditCard = (creditCard: string) => {
+  if (creditCard.length < 6) return creditCard;
+  const last4Characters = creditCard.substr(-4);
+  const maskingCharacters = creditCard.substr(0, creditCard.length - 5).replace(/\d/g, '#');
+  return `${maskingCharacters}${last4Characters}`;
+}
