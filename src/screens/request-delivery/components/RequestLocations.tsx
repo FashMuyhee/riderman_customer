8cd0f5d0bfa2348de3baa5169c19c20a0a3f6a6a
Fash Muyhee <fashmuyidavid@gmail.com>
2022-08-05 11:24:05 +0100

    add more props to support delivery history

--- src/screens/request-delivery/components/RequestLocations.tsx
+++ src/screens/request-delivery/components/RequestLocations.tsx
@@ -2,21 +2,27 @@ import LocationPinIcon from '@components/icons/location-pin';
 import LocationRadiusIcon from '@components/icons/location-radius';
 import {hp} from '@utils/responsive';
 import {View, Text, VStack, HStack} from 'native-base';
+import {ColorType} from 'native-base/lib/typescript/components/types';
 import React from 'react';
 
 type RequestLocationsProps = {
   pickUp: string;
   deliveryLocations: Array<string>;
+  bg?: ColorType;
+  mt?: string;
 };
 
-const RequestLocations = ({deliveryLocations, pickUp}: RequestLocationsProps) => {
+/**
+ * Request Location Component
+ */
+const RequestLocations = ({deliveryLocations, pickUp, bg, mt = '5%'}: RequestLocationsProps) => {
   return (
-    <HStack alignItems="flex-start" space="2" justifyContent="flex-start" mt="5%" px="10px">
+    <HStack bg={bg} alignItems="flex-start" space="2" justifyContent="flex-start" mt={mt} px="10px">
       <VStack justifyContent="center" alignItems="center">
         <LocationRadiusIcon />
         {deliveryLocations.map((item, key) => (
           <View key={key}>
-            <View bg="accent" alignSelf="center" h="15px" w="2px"/>
+            <View bg="accent" alignSelf="center" h="15px" w="2px" />
             <LocationPinIcon />
           </View>
         ))}
