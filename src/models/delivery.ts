af162fdec68849f1934e7c0e997ce1ab5d8e4f3d
Fash Muyhee <fashmuyidavid@gmail.com>
2022-08-07 09:55:04 +0100

    notification screen

--- src/models/delivery.ts
+++ src/models/delivery.ts
@@ -1,6 +1,7 @@
 import { LocationValue } from "@screens/request-delivery/request-delivery";
 
 export type PaymentMethod = "Cash" | 'Card' | 'Wallet'
+export type DeliveryStatus = 'Processing' | 'Confirmed' | 'Completed' | 'Active'
 
 export type IDeliveryRequestBody = {
   pickupLocation: LocationValue;
