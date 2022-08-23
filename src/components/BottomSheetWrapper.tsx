cdc18fdfbc8e463113ff287486afb2bf6b616742
Fash Muyhee <fashmuyidavid@gmail.com>
2022-08-23 21:51:53 +0100

    add bgColor prop to snappy bottom sheet

--- src/components/BottomSheetWrapper.tsx
+++ src/components/BottomSheetWrapper.tsx
@@ -1,9 +1,8 @@
 import React, {useCallback} from 'react';
 import RBSheet from 'react-native-raw-bottom-sheet';
-import {hp, wp} from '@utils/responsive';
+import {hp} from '@utils/responsive';
 import BottomSheet, {BottomSheetBackdrop, BottomSheetBackdropProps} from '@gorhom/bottom-sheet';
-import {Modal} from 'react-native';
-import {View} from 'native-base';
+import {ColorType} from 'native-base/lib/typescript/components/types';
 
 export type IBottomSheetWrapperProps = {
   isBackDrop?: boolean;
@@ -48,21 +47,13 @@ export type IBottomSheetWrapperSnappyProps = {
   isCustomBackDrop?: boolean;
   customBackDrop?: React.FC<BottomSheetBackdropProps>;
   showBackdrop?: boolean;
+  bgColor?: ColorType;
 };
 
 const BottomSheetWrapperSnappy = React.forwardRef<BottomSheet, IBottomSheetWrapperSnappyProps>(
-  ({children, customBackDrop, isCustomBackDrop, snapPoints = ['25%', '50%'], index = -1, dragClose = true, noIndicator, showBackdrop}, ref) => {
+  ({children, customBackDrop, isCustomBackDrop, snapPoints = ['25%', '50%'], index = -1, dragClose = true, noIndicator, showBackdrop, bgColor = 'white'}, ref) => {
     const renderBackdrop = useCallback(props => <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={0} opacity={0.2} />, []);
 
-    // const CustomBackDrop = () => {
-    //   return (
-    //     <Modal visible transparent>
-    //       <View w={wp(100)} h={hp(100)}>
-    //         {customBackDrop}
-    //       </View>
-    //     </Modal>
-    //   );
-    // };
     return (
       <BottomSheet
         backdropComponent={showBackdrop ? renderBackdrop : undefined}
@@ -71,10 +62,11 @@ const BottomSheetWrapperSnappy = React.forwardRef<BottomSheet, IBottomSheetWrapp
         android_keyboardInputMode="adjustResize"
         enablePanDownToClose={dragClose}
         handleIndicatorStyle={{backgroundColor: '#f5f5f5', width: noIndicator ? 0 : 60}}
-        backgroundStyle={{backgroundColor: '#fff'}}
+        // @ts-ignore
+        backgroundStyle={{backgroundColor: bgColor}}
         index={index}
         ref={ref}
-        animationConfigs={{damping: 20, stiffness: 90,}}
+        animationConfigs={{damping: 20, stiffness: 150}}
         snapPoints={snapPoints}>
         {children}
       </BottomSheet>
