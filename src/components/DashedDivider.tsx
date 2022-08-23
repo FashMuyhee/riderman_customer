bab71840fe2e16d4e5c1c1235a9c339cc7ad0c1a
Fash Muyhee <fashmuyidavid@gmail.com>
2022-08-23 21:51:34 +0100

    add mt prop

--- src/components/DashedDivider.tsx
+++ src/components/DashedDivider.tsx
@@ -1,10 +1,8 @@
 import {View} from 'native-base';
 import React from 'react';
 
-type Props = {};
-
-const DashedDivider = (props: Props) => {
-  return <View borderWidth={1} mx="10px" mt="10px" borderColor="gray.200" borderStyle="dashed" />;
+const DashedDivider = ({mt = '10px'}: {mt?: string}) => {
+  return <View borderWidth={1} mx="10px" mt={mt} borderColor="gray.200" borderStyle="dashed" />;
 };
 
 export default DashedDivider;
