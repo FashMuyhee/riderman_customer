4822416bbd10025b18a1f05f0cf78e48313e3f96
Fash Muyhee <fashmuyidavid@gmail.com>
2022-11-16 19:26:03 +0100

    add defined duration props

--- src/components/RenderSnackbar.ts
+++ src/components/RenderSnackbar.ts
@@ -1,13 +1,28 @@
 import { FONT } from "@utils/constant";
 import Snackbar, { SnackBarOptions } from "react-native-snackbar";
 
+interface RenderSnackbar extends Omit<SnackBarOptions, 'duration'> {
+  duration?: 'INFINITE' | 'LONG' | 'SHORT'
+}
 /**
  * render snackbar with customization
- * @param  {SnackBarOptions} params
+ * @param  {RenderSnackbar} params
  */
-const RenderSnackbar = (params: SnackBarOptions) => {
+const RenderSnackbar = (params: RenderSnackbar) => {
+  const dismissTime = () => {
+    switch (params.duration) {
+      case 'INFINITE':
+        return Snackbar.LENGTH_INDEFINITE;
+      case 'LONG':
+        return Snackbar.LENGTH_LONG;
+      case 'SHORT':
+        return Snackbar.LENGTH_SHORT;
+      default:
+        return Snackbar.LENGTH_LONG;
+    }
+  }
   return Snackbar.show({
-    ...params, fontFamily: FONT.REGULAR, textColor: 'white'
+    ...params, fontFamily: FONT.REGULAR, textColor: 'white', duration: dismissTime()
   })
 }
 
