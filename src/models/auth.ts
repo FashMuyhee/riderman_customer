75d8d1b7061e177a2e3607e52d25ba761ff40f5b
Fash Muyhee <fashmuyidavid@gmail.com>
2022-11-16 20:00:31 +0100

    aut completed

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,18 +1,17 @@
+export type StatusCode = 200 | 400 | 201
 export interface IStoreUserSession {
   token: string,
-  expireTime: string,
-  refresh: string,
-  refreshTokenExpireTime: string
-
+  expireTime: number,
 }
 
 export interface IStoreUserCredentials {
   email: string;
   password: string;
-  uid: string;
+  uid: number;
 
 }
 
+export type Channel = 'sms' | 'email'
 export interface ILoginForm {
   email: string;
   password: string
@@ -26,6 +25,15 @@ export interface IRegisterForm {
   password: string
 }
 
+export interface IUser {
+  id: number,
+  first_name: string,
+  last_name: string,
+  phone: string,
+  email: string,
+  verified: number,
+  image: string | null,
+}
 
 export interface IVerifyTokenForm {
   phone: string;
@@ -36,4 +44,19 @@ export interface IRsetPasswordForm {
   phone: string;
   token: string;
   newPassword: string
+}
+
+export interface ILoginRegisterResponse {
+  success: boolean,
+  message: string,
+  data?: IUser,
+  token?: string,
+  statusCode: StatusCode
+  errors?: Record<string, string[]>
+}
+
+export interface IGeneralResponse {
+  message: string;
+  success: boolean
+  statusCode: StatusCode
 }
--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,8 +1,14 @@
 import httpHandler, { configureAxiosHeaders } from '../utils/http';
-import { ILoginForm, IRegisterForm, IRsetPasswordForm, IVerifyTokenForm } from '@models/auth';
+import { ILoginForm, IRegisterForm, ILoginRegisterResponse, IRsetPasswordForm, IVerifyTokenForm, IGeneralResponse, Channel } from '@models/auth';
 import tokenManagerService from './TokenManager';
 
 class AuthService {
+
+  /**
+   * login 
+   * @param  {} {email
+   * @param  {ILoginForm} password}
+   */
   async login({ email, password }: ILoginForm) {
     try {
       const result = await httpHandler({
@@ -10,34 +16,30 @@ class AuthService {
         url: '/login',
         data: { email, password },
       });
-      const data: AuthUser = result.data;
-
-      if (data.status) {
-        const { accessToken, accessTokenExpiresIn, refreshToken, employeeID, refreshTokenExpiresIn, companyID } = data?.data || {};
+      const data: ILoginRegisterResponse = result.data;
+      if (data.statusCode === 200) {
         // @ts-ignore set token
-        configureAxiosHeaders(accessToken);
+        configureAxiosHeaders(data?.token);
         tokenManagerService.storeUserSession({
-          token: accessToken,
-          expireTime: accessTokenExpiresIn,
-          refresh: refreshToken,
-          refreshTokenExpireTime: refreshTokenExpiresIn,
+          token: data?.token as string,
+          expireTime: 111
         });
-
         tokenManagerService.storeUserCredentials({
           email,
           password,
-          uid: employeeID,
+          uid: data?.data?.id as number,
         });
+        return data
       }
-      return data
     } catch (error) {
-      const message: AuthUser = {
+      const message: ILoginRegisterResponse = {
         //@ts-ignore
         message: error?.response.data.message,
-        status: false,
-        data: undefined
+        success: false,
+        //@ts-ignore
+        statusCode: error?.response?.data?.statusCode
       };
-      return { data: message, modules: [] };
+      return message
     }
   }
 
@@ -52,19 +54,69 @@ class AuthService {
         url: '/customer/register',
         data: body,
       });
-      const data: RegisterUserResponse = result.data;
-      if (result.status) {
-        tokenManagerService.storeUserCredentials({ email: body.email, password: body.password })
+      const data: ILoginRegisterResponse = result.data;
+      if (data.success) {
+        tokenManagerService.storeUserCredentials({ email: body.email, password: body.password, uid: data?.data?.id as number })
+        // @ts-ignore set token
+        configureAxiosHeaders(data?.token);
+        return data;
       }
-      return data;
     } catch (error) {
-      //@ts-ignore
-      // console.log(error.response);
-      const message: RegisterUserResponse = {
+      const message: ILoginRegisterResponse = {
         //@ts-ignore
         message: error?.response.data.message,
-        status: false,
-        data: null,
+        success: false,
+        //@ts-ignore
+        statusCode: error?.response?.data?.statusCode
+      };
+      return message;
+    }
+  }
+
+  /**
+   * resend account verification token
+   */
+  async resendAccountVerify(channel: Channel = 'email') {
+    try {
+      const result = await httpHandler({
+        method: 'post',
+        url: `/verify-user/resend-token/${channel}`,
+      });
+      const data: IGeneralResponse = result.data;
+      if (data.success) {
+        return data;
+      }
+    } catch (error) {
+      //@ts-ignore
+      const message: IGeneralResponse = {
+        //@ts-ignore
+        message: error?.response.data.message,
+        success: false,
+      };
+      return message;
+    }
+  }
+
+  /**
+   * resend account verification token
+   */
+  async verifyAccount(channel: Channel = 'email', token: string) {
+    try {
+      const result = await httpHandler({
+        method: 'post',
+        url: `/verify-user/${channel}`,
+        data: { token }
+      });
+      const data: IGeneralResponse = result.data;
+      if (data.success) {
+        return data;
+      }
+    } catch (error) {
+      //@ts-ignore
+      const message: IGeneralResponse = {
+        //@ts-ignore
+        message: error?.response.data.message,
+        success: false,
       };
       return message;
     }
@@ -74,24 +126,24 @@ class AuthService {
    * send token for forget password
    * @param  {{email:string}} {email}
    */
-  async sendForgotPasswordToken({ phone }: { phone: string }) {
+  async sendForgotPasswordToken(body: { phone: string }, channel: Channel = 'email') {
     try {
       const result = await httpHandler({
         method: 'post',
-        url: '/forgot-password/send-token/sms',
-        data: { phone },
+        url: `/forgot-password/send-token/${channel}`,
+        data: body
       });
-      const data: ForgotPassword = result.data;
+      const data: IGeneralResponse = result.data;
 
-      if (data.status) {
+      if (data.statusCode === 200) {
         return data;
       }
     } catch (error) {
-      //@ts-ignore
-      const message: ForgotPassword = {
+      const message: IGeneralResponse = {
         //@ts-ignore
         message: error?.response.data.message,
-        status: false,
+        success: false,
+        statusCode: 400
       };
       return message;
     }
@@ -101,49 +153,48 @@ class AuthService {
    * send token for forget password
    * @param  {{email:string}} {email}
    */
-  async verifyToken(body: IVerifyTokenForm) {
+  async verifyForgetPasswordToken(body: IVerifyTokenForm, channel: Channel = 'email') {
     try {
       const result = await httpHandler({
         method: 'post',
-        url: '/forgot-password/verify-token/sms',
+        url: `/forgot-password/verify-token/${channel}`,
         data: body,
       });
-      const data: ForgotPassword = result.data;
+      const data: IGeneralResponse = result.data;
 
-      if (data.status) {
+      if (data.success) {
         return data;
       }
     } catch (error) {
-      //@ts-ignore
-      const message: ForgotPassword = {
+      const message: IGeneralResponse = {
         //@ts-ignore
         message: error?.response.data.message,
-        status: false,
+        success: false,
+        statusCode: 400
       };
       return message;
     }
   }
 
 
-  async resetPassword(body: IRsetPasswordForm) {
+  async resetPassword(body: IRsetPasswordForm, channel: Channel = 'email') {
     try {
       const result = await httpHandler({
         method: 'post',
-        url: `/forgot-password/verify-token/sms'`,
+        url: `/forgot-password/reset/${channel}`,
         data: body,
       });
-      const data: ForgotPassword = result.data;
+      const data: IGeneralResponse = result.data;
 
-      if (data.status) {
+      if (data.statusCode == 200) {
         return data;
       }
     } catch (error) {
-      //@ts-ignore
-      console.log(error.response);
-      const message: ForgotPassword = {
+      const message: IGeneralResponse = {
         //@ts-ignore
         message: error?.response.data.message,
-        status: false,
+        statusCode: 400,
+        success: false
       };
       return message;
     }
