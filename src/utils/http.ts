b306860244806190632659c74db9d044aa08b4c6
Fash Muyhee <fashmuyidavid@gmail.com>
2023-01-21 12:59:29 +0100

    fix http token issue

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,8 @@
 import tokenManagerService from '@services/TokenManager';
-import axios, { AxiosInstance } from 'axios';
+import axios, {AxiosInstance} from 'axios';
 import axiosRetry from 'axios-retry';
 
-export const BASE_URL = 'https://riderman-dev.herokuapp.com/api/v1'
+export const BASE_URL = 'https://riderman-dev.herokuapp.com/api/v1';
 
 /**
  * axios instance
@@ -22,41 +22,39 @@ const httpHandler: AxiosInstance = axios.create({
  * Axios retry config
  */
 axiosRetry(httpHandler, {
-  retries: 10, retryDelay: retryCount => {
-    return retryCount * 2000
+  retries: 10,
+  retryDelay: retryCount => {
+    return retryCount * 2000;
   },
 });
 
 /**
  * set axios token to header
- * @param token 
+ * @param token
  */
 export const configureAxiosHeaders = async (token: string) => {
   httpHandler.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   httpHandler.defaults.headers.common['Content-Type'] = 'application/json';
-  console.log('axois token set');
 };
 
-// // intercept request config
-// httpHandler.interceptors.request.use(async (config) => {
-//   const session = await isAccessTokenExpired()
-
-//   if (!config?.headers?.Authorization) {
-//     if (session === 'SESSION_ACTIVE') {
-//       const userSession = tokenManagerService.retrieveUserSession();
-//       // @ts-ignore
-//       config.headers.common['Authorization'] = userSession !== null ? `Bearer ${userSession?.token}` : ''
-//     } else if (session === 'EMPTY_SESSION') {
-//       // @ts-ignore
-//       config.headers.common['Authorization'] = ''
-//     } else {
-//       //TODO: logout or something
-//     }
-//   }
-//   return config;
-// }, function (error) {
-//   return Promise.reject(error);
-// });
-
+httpHandler.interceptors.request.use(
+  async config => {
+    const token = tokenManagerService.retrieveUserSession();
+    if (!config?.headers?.Authorization) {
+      if (!!token?.token) {
+        // @ts-ignore
+        config.headers['Authorization'] = `Bearer ${token?.token}`;
+      } else {
+        // @ts-ignore
+        config.headers['Authorization'] = '';
+        //TODO: logout or something
+      }
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 
 export default httpHandler;
