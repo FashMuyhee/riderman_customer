eb411e378043c8e0708f0ba2962b7626d9662afa
Fash Muyhee <fashmuyidavid@gmail.com>
2023-02-05 22:34:53 +0100

    add cordinate  prop

--- src/screens/request-delivery/components/MapSection.tsx
+++ src/screens/request-delivery/components/MapSection.tsx
@@ -1,25 +1,24 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {IconButton, Image, useTheme, View} from 'native-base';
+import React, {useRef} from 'react';
+import { useTheme, View} from 'native-base';
 import {hp} from '@utils/responsive';
 import MapView, {Marker} from 'react-native-maps';
 import {StyleSheet} from 'react-native';
-import useGetLocation from '@hooks/useGetLocation';
 import mapStyle from '@utils/map-style';
-import {bikes} from '@utils/sample-data';
-import bikeIllus from '@images/icons/bike.png';
 // @ts-ignore
 import {G_MAP_KEY} from '@env';
 import MapViewDirections from 'react-native-maps-directions';
 
 export type IMapSectionProps = {
   height?: number;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  }[];
 };
 
-const MapSection: React.FC<IMapSectionProps> = ({height = hp(40)}) => {
-  const {location, loading, getLocation} = useGetLocation();
-  const {colors} = useTheme();
-  const mapRef = useRef<MapView>(null);
-  const [coordinates] = useState([
+const MapSection: React.FC<IMapSectionProps> = ({
+  height = hp(40),
+  coordinates = [
     {
       latitude: 6.545654504207982,
       longitude: 3.3955299324844184,
@@ -28,24 +27,27 @@ const MapSection: React.FC<IMapSectionProps> = ({height = hp(40)}) => {
       latitude: 6.544355082389553,
       longitude: 3.395295243282711,
     },
-  ]);
+  ],
+}) => {
+  const {colors} = useTheme();
+  const mapRef = useRef<MapView>(null);
 
   return (
     <View h={height} w="full">
       <MapView
         ref={mapRef}
         initialRegion={{
-          latitude: coordinates[0].latitude,
-          longitude: coordinates[0].longitude,
+          latitude: coordinates[0]?.latitude,
+          longitude: coordinates[0]?.longitude,
           latitudeDelta: 0.0622,
           longitudeDelta: 0.0121,
         }}
         provider="google"
         style={StyleSheet.absoluteFill}
-        minZoomLevel={18}
+        minZoomLevel={5}
         customMapStyle={mapStyle}
         rotateEnabled={false}
-        loadingEnabled={loading}
+        loadingEnabled
         loadingIndicatorColor={colors.main}>
         <MapViewDirections origin={coordinates[0]} destination={coordinates[1]} apikey={G_MAP_KEY} strokeWidth={4} strokeColor={colors.main} />
         <Marker coordinate={coordinates[0]} />
