3172c9e9e3355411952faa01da9d70b0ef625bfc
Fash Muyhee <fashmuyidavid@gmail.com>
2023-02-05 22:38:47 +0100

    more types

--- src/models/delivery.ts
+++ src/models/delivery.ts
@@ -23,7 +23,7 @@ export type LocationValue = {
 export type IDeliveryRequestBody = {
   pickupLocation: LocationValue;
   deliveryLocation: LocationValue;
-  packageName: string;
+  instruction: string;
   packageTypes: string[];
   weight: string;
   packageNo: string;
@@ -58,15 +58,74 @@ export type DeliveryPackage = {
   distanceFromPickup: string;
 };
 
+export type RiderCloseBy = {
+  riderId: string;
+  user: IUser;
+  company: {
+    companyId: number;
+    name: string;
+    deliveryRates: {
+      perKg: number;
+      perKm: number;
+    };
+    termsAndConditions: string;
+    totalPrice: string;
+  };
+  bikeDetails: BikeDetails;
+  status: 'active' | 'inactive';
+  state: 'active' | 'offline' | 'busy';
+  location: LocationValue;
+  rating: string;
+  completedDeliveries: number;
+  createdAt: Date;
+};
+
+export type PickupRequestInfo = {
+  pickupRequestId: number;
+  riderId: RiderCloseBy;
+  pickupLocation: LocationValue;
+  delivery_locations: LocationValue[];
+  delivery_packages: DeliveryPackage[];
+  paymentChannel: PaymentMethod;
+  totalDistance: string;
+  totalAmount: number;
+  status: DeliveryStatus;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 export type PickupFormBody = {
   pickupLocation: LocationValue;
   deliveryLocations: LocationValue[];
   deliveryPackages: DeliveryPackage[];
   totalDistance: string;
 };
+
+export type ConfirmPickupFormBody = {
+  paymentChannel: PaymentMethod;
+  totalAmount: string;
+  riderId: string;
+  pickupRequestId: string;
+};
 export interface RiderResponse {
   message: string;
   success: boolean;
   statusCode: StatusCode;
   data: Array<IRider>;
+}
+export interface CreatePickupRequestResponse {
+  message: string;
+  success: boolean;
+  statusCode: StatusCode;
+  data: PickupRequestInfo;
+}
+
+export interface IRiderCloseByResponse {
+  message: string;
+  success: boolean;
+  statusCode: StatusCode;
+  data: {
+    pickupRequestId: string;
+    riders: Array<RiderCloseBy>;
+  };
 }
