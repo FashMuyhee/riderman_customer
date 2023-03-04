1b0ed26d9969598710d5e0e088c917137299ddba
Fash Muyhee <fashmuyidavid@gmail.com>
2023-03-04 22:06:50 +0100

    cleaned

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,14 @@
-import { configureStore, combineReducers, ConfigureStoreOptions } from '@reduxjs/toolkit';
-import { deliveryApi } from '@services/rtk-queries/deliveries';
-import { riderApi } from '@services/rtk-queries/rider';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {configureStore, combineReducers, ConfigureStoreOptions} from '@reduxjs/toolkit';
+import {deliveryApi} from '@services/rtk-queries/deliveries';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
-
-export const createStore = (
-  options?: ConfigureStoreOptions["preloadedState"] | undefined
-) =>
+export const createStore = (options?: ConfigureStoreOptions['preloadedState'] | undefined) =>
   configureStore({
     reducer: {
-      [riderApi.reducerPath]: riderApi.reducer,
-      [deliveryApi.reducerPath]: deliveryApi.reducer
+      [deliveryApi.reducerPath]: deliveryApi.reducer,
     },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(riderApi.middleware, deliveryApi.middleware),
-    ...options
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(deliveryApi.middleware),
+    ...options,
   });
 
 export const store = createStore();
