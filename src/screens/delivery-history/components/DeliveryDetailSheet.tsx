2ed494c90ef4c627fc50f3148f9f1b5c397ae151
Fash Muyhee <fashmuyidavid@gmail.com>
2023-03-04 22:37:49 +0100

    delivery cinfirmation

--- src/screens/delivery-history/components/DeliveryDetailSheet.tsx
+++ src/screens/delivery-history/components/DeliveryDetailSheet.tsx
@@ -5,7 +5,7 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import {hp} from '@utils/responsive';
 import companyLogo from '@images/company-logo.png';
 import nairaCoin from '@images/icons/naira-coin.png';
-import {Button, DashedDivider, MoneyText} from '@components';
+import {Button, DashedDivider, MoneyText, RenderSnackbar} from '@components';
 import TimeSolid from '@components/icons/time-solid';
 import {PaymentMethodIcon} from '@screens/request-delivery/components/SelectPaymentMethod';
 import RiderInfo from '@screens/request-delivery/components/RiderInfo';
@@ -15,6 +15,7 @@ import CallIcon from '@components/icons/call';
 import {DeliveryStatus, IDeliveryItem} from '@models/delivery';
 import {Image as RNImage} from 'react-native';
 import openDialer from '@utils/open-dialer';
+import {useConfirmDeliveryMutation} from '@services/rtk-queries/deliveries';
 
 type Props = {
   item: IDeliveryItem;
@@ -23,100 +24,161 @@ type Props = {
   handleTipRider: () => void;
 };
 
-const DeliveryDetailSheet = React.forwardRef<BottomSheet, Props>(({onClose, deliveryStatus, handleTipRider, item}, ref) => {
-  const snapPoints = useMemo(() => ['65%', '80%'], []);
-  const {rider, pickupRequest, deliveryStatus: status} = item || {};
+const DeliveryDetailSheet = React.forwardRef<BottomSheet, Props>(
+  ({onClose, deliveryStatus, handleTipRider, item}, ref) => {
+    const snapPoints = useMemo(() => ['65%', '80%'], []);
+    const {rider, pickupRequest, deliveryId} = item || {};
+    const [confirmDelivery, {isLoading}] = useConfirmDeliveryMutation();
 
-  const callRider = () => {
-    openDialer(rider.user.phone);
-  };
+    const callRider = () => {
+      openDialer(rider.user.phone);
+    };
 
-  const renderButtons = () => {
-    if (deliveryStatus === 'active' || deliveryStatus === 'processing') {
-      return <Button title="Call Rider" leftIcon={<CallIcon />} />;
-    }
+    const handleConfirm = async () => {
+      try {
+        const res = await confirmDelivery(deliveryId.toString()).unwrap();
+        if (res.success) {
+          RenderSnackbar({text: res.message});
+          onClose();
+        }
+      } catch (error) {
+        RenderSnackbar({
+          text: `Sorry ,we couldn't confirm this package, Tr yagain`,
+        });
+      }
+    };
 
-    if (deliveryStatus === 'completed') {
+    const renderButtons = () => {
+      if (deliveryStatus === 'active' || deliveryStatus === 'processing') {
+        return <Button title="Call Rider" leftIcon={<CallIcon />} />;
+      }
+
+      if (deliveryStatus === 'completed') {
+        return (
+          <HStack
+            alignItems="center"
+            justifyContent="space-between"
+            mt="5%"
+            px="10px">
+            <Button
+              title="Call Rider"
+              onPress={callRider}
+              w="48%"
+              leftIcon={<CallIcon />}
+              isDisabled={isLoading}
+            />
+            <Button
+              bg="black"
+              isLoading={isLoading}
+              onPress={handleConfirm}
+              title="Received"
+              w="48%"
+            />
+          </HStack>
+        );
+      }
+
       return (
-        <HStack alignItems="center" justifyContent="space-between" mt="5%" px="10px">
-          <Button title="Call Rider" onPress={callRider} w="48%" leftIcon={<CallIcon />} />
-          <Button bg="black" onPress={onClose} title="Received" w="48%" />
-        </HStack>
+        <Button
+          onPress={handleTipRider}
+          title="Tip Rider"
+          leftIcon={
+            <RNImage
+              source={nairaCoin}
+              style={{width: 30, height: 30, resizeMode: 'contain'}}
+            />
+          }
+        />
       );
-    }
+    };
+
+    const locations = pickupRequest?.deliveryLocations;
+    const deliveryLocations =
+      !!locations &&
+      locations?.map(item => {
+        return item.address;
+      });
 
     return (
-      <Button
-        onPress={handleTipRider}
-        title="Tip Rider"
-        leftIcon={<RNImage source={nairaCoin} style={{width: 30, height: 30, resizeMode: 'contain'}} />}
-      />
-    );
-  };
-
-  const locations = pickupRequest?.deliveryLocations;
-  const deliveryLocations =
-    !!locations &&
-    locations?.map(item => {
-      return item.address;
-    });
-
-  return (
-    <BottomSheetWrapperSnappy noIndicator showBackdrop index={-1} ref={ref} snapPoints={snapPoints}>
-      <View px="20px" w="full">
-        {/* company info */}
-        <HStack h="60px" alignItems="center" justifyContent="space-between" px="10px">
-          {/* TODO company logo */}
-          <Image source={companyLogo} alt={`${rider?.companyName}_logo`} rounded="full" bg="gray.400" size="50px" />
-          <View w="60%">
-            <Text fontSize={hp(1.3)} fontWeight="600">
-              {rider?.companyName}
-            </Text>
-            <HStack mt="10px" alignItems="center" space="2">
-              <TimeSolid />
-              <Text fontSize="12px">3 min away</Text>
-            </HStack>
-          </View>
-          <View justifyContent="center" alignItems="center" w="20%">
-            <HStack alignItems="center" space="1">
-              <PaymentMethodIcon method={pickupRequest?.paymentChannel} selected={false} />
-              <Text bold textTransform="capitalize">
-                {pickupRequest?.paymentChannel}
-              </Text>
-            </HStack>
-            <MoneyText bold moneyValue={pickupRequest.totalAmount} />
-          </View>
-        </HStack>
-        <DashedDivider />
-        <View>
-          {/* rider info */}
-          <RiderInfo
-            plateNo={rider?.bikeDetails.licenseNumber}
-            image={{uri: rider?.user?.image as string}}
-            rating={rider?.rating}
-            fullname={`${rider?.user.firstName} ${rider?.user.lastName}`}
-          />
-          {/* delivery location */}
-          <RequestLocations deliveryLocations={deliveryLocations} pickUp={pickupRequest?.pickupLocation.address} />
-        </View>
-        <DashedDivider />
-        {!!pickupRequest?.deliveryPackages &&
-          pickupRequest.deliveryPackages.map((x, y) => (
-            <PackageDetail
-              instruction={x.deliveryInstructions}
-              index={y}
-              contactName={x.recipient.name}
-              contactPhone={x.recipient.phone}
-              packageType={x.packageCategories}
+      <BottomSheetWrapperSnappy
+        noIndicator
+        showBackdrop
+        index={-1}
+        ref={ref}
+        snapPoints={snapPoints}>
+        <View px="20px" w="full">
+          {/* company info */}
+          <HStack
+            h="60px"
+            alignItems="center"
+            justifyContent="space-between"
+            px="10px">
+            {/* TODO company logo */}
+            <Image
+              source={companyLogo}
+              alt={`${rider?.companyName}_logo`}
+              rounded="full"
+              bg="gray.400"
+              size="50px"
             />
-          ))}
-        <DashedDivider />
-        <View mt="20px" px="10px">
-          {renderButtons()}
+            <View w="60%">
+              <Text fontSize={hp(1.3)} fontWeight="600">
+                {rider?.companyName}
+              </Text>
+              <HStack mt="10px" alignItems="center" space="2">
+                {/* TODO : live timing*/}
+                <TimeSolid />
+                <Text fontSize="12px">3 min away</Text>
+              </HStack>
+            </View>
+            <View justifyContent="center" alignItems="center" w="20%">
+              <HStack alignItems="center" space="1">
+                <PaymentMethodIcon
+                  method={pickupRequest?.paymentChannel}
+                  selected={false}
+                />
+                <Text bold textTransform="capitalize">
+                  {pickupRequest?.paymentChannel}
+                </Text>
+              </HStack>
+              <MoneyText bold moneyValue={pickupRequest?.totalAmount} />
+            </View>
+          </HStack>
+          <DashedDivider />
+          <View>
+            {/* rider info */}
+            <RiderInfo
+              plateNo={rider?.bikeDetails.licenseNumber}
+              image={{uri: rider?.user?.image as string}}
+              rating={rider?.rating}
+              fullname={`${rider?.user.firstName} ${rider?.user.lastName}`}
+            />
+            {/* delivery location */}
+            <RequestLocations
+              deliveryLocations={deliveryLocations}
+              pickUp={pickupRequest?.pickupLocation.address}
+            />
+          </View>
+          <DashedDivider />
+          {!!pickupRequest?.deliveryPackages &&
+            pickupRequest.deliveryPackages.map((x, y) => (
+              <PackageDetail
+                instruction={x.deliveryInstructions}
+                index={y}
+                key={`PackageDetail_${y}`}
+                contactName={x.recipient.name}
+                contactPhone={x.recipient.phone}
+                packageType={x.packageCategories}
+              />
+            ))}
+          <DashedDivider />
+          <View mt="20px" px="10px">
+            {renderButtons()}
+          </View>
         </View>
-      </View>
-    </BottomSheetWrapperSnappy>
-  );
-});
+      </BottomSheetWrapperSnappy>
+    );
+  },
+);
 
 export default DeliveryDetailSheet;
--- src/services/rtk-queries/deliveries.ts
+++ src/services/rtk-queries/deliveries.ts
@@ -1,10 +1,14 @@
-import { DeliveryStatus, IDeliveriesResponse, IDeliveryResponse } from '@models/delivery';
-import { createApi } from '@reduxjs/toolkit/query/react';
-import { axiosBaseQuery } from '@utils/http';
+import {
+  DeliveryStatus,
+  IDeliveriesResponse,
+  IDeliveryResponse,
+} from '@models/delivery';
+import {createApi} from '@reduxjs/toolkit/query/react';
+import {axiosBaseQuery} from '@utils/http';
 
 export const deliveryApi = createApi({
   reducerPath: 'deliveryApi',
-  baseQuery: axiosBaseQuery({ baseUrl: '/customer/deliveries' }),
+  baseQuery: axiosBaseQuery({baseUrl: '/customer/deliveries'}),
   tagTypes: ['DeliveryRequest'],
   endpoints: builder => ({
     getDeliveries: builder.query<IDeliveriesResponse, number>({
@@ -14,15 +18,21 @@ export const deliveryApi = createApi({
           method: 'GET',
         };
       },
-      providesTags: (result) =>
+      providesTags: result =>
         result
           ? [
-            ...result.data.map(({ deliveryId }) => ({ type: 'DeliveryRequest' as const, id: deliveryId })),
-            { type: 'DeliveryRequest', id: 'ALL_DELIVERIES' },
-          ]
-          : [{ type: 'DeliveryRequest', id: 'ALL_DELIVERIES' }],
+              ...result.data.map(({deliveryId}) => ({
+                type: 'DeliveryRequest' as const,
+                id: deliveryId,
+              })),
+              {type: 'DeliveryRequest', id: 'ALL_DELIVERIES'},
+            ]
+          : [{type: 'DeliveryRequest', id: 'ALL_DELIVERIES'}],
     }),
-    getDeliveriesByStatus: builder.query<IDeliveriesResponse, { page: number, status: DeliveryStatus }>({
+    getDeliveriesByStatus: builder.query<
+      IDeliveriesResponse,
+      {page: number; status: DeliveryStatus}
+    >({
       query(req) {
         return {
           url: `?status=${req.status.toLocaleLowerCase()}&page=${req.page}`,
@@ -32,21 +42,46 @@ export const deliveryApi = createApi({
       providesTags: (result, _, req) =>
         result
           ? [
-            ...result.data.map(({ deliveryId }) => ({ type: 'DeliveryRequest' as const, id: deliveryId })),
-            { type: 'DeliveryRequest', id: `${req.status.toUpperCase()}_DELIVERIES` },
-          ]
-          : [{ type: 'DeliveryRequest', id: `${req.status.toUpperCase()}_DELIVERIES` }],
+              ...result.data.map(({deliveryId}) => ({
+                type: 'DeliveryRequest' as const,
+                id: deliveryId,
+              })),
+              {
+                type: 'DeliveryRequest',
+                id: `${req.status.toUpperCase()}_DELIVERIES`,
+              },
+            ]
+          : [
+              {
+                type: 'DeliveryRequest',
+                id: `${req.status.toUpperCase()}_DELIVERIES`,
+              },
+            ],
     }),
-    getSingleDelivery: builder.mutation<IDeliveryResponse, string>({
+    getSingleDelivery: builder.query<IDeliveryResponse, string>({
       query(id) {
         return {
           url: `/${id}`,
           method: 'GET',
         };
       },
-      invalidatesTags: (__, _, req) => [{ type: 'DeliveryRequest', id: req }],
+      providesTags: (__, _, req) => [{type: 'DeliveryRequest', id: req}],
+    }),
+    confirmDelivery: builder.mutation<IDeliveryResponse, string>({
+      query(id) {
+        return {
+          url: `/${id}/status/confirmed`,
+          method: 'PATCH',
+        };
+      },
+      invalidatesTags: (__, _, req) => [{type: 'DeliveryRequest', id: req}],
     }),
   }),
 });
 
-export const { useGetDeliveriesByStatusQuery, useGetDeliveriesQuery, useGetSingleDeliveryMutation } = deliveryApi;
+export const {
+  useGetDeliveriesByStatusQuery,
+  useGetDeliveriesQuery,
+  useGetSingleDeliveryQuery,
+  useConfirmDeliveryMutation,
+} = deliveryApi;
